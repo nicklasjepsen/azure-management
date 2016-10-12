b7026b716335d627597469b0bb55a095bbaadb1f
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-10-12 11:45:41 +0200

    Initial commit

---
+++ AzureManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureManagement.Global" Language="C#" %>
---
+++ AzureManagement/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace AzureManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
